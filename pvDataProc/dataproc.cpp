2e46b5282acabcca7ff5e99208b3d7c5951e6487
JJMG Engenharia <jjmg.engenharia@gmail.com>
2025-04-14 16:38:54 -0300

    Update dataproc.cpp

--- pvDataProc/dataproc.cpp
+++ pvDataProc/dataproc.cpp
@@ -23,17 +23,22 @@ int main(){
 
 void setup()
 {
-	int i,j;
+    int i,j;
     
     mbox.clear();
 
-	db.open("QMYSQL","localhost","pvdb","pvbrowser","pvbrowser");
+    db.open("QMYSQL","localhost","pvdb","pvbrowser","pvbrowser");
 
-	i=sprintf(buf,"create table if not exists %s (t timestamp(3)",tableName);
-	for (j=0; j<2; j++) i+=sprintf(&buf[i],",F%d float", j+1);
+    i=sprintf(buf,"create table if not exists %s (t timestamp(3)",tableName);
+    for (j=0; j<2; j++) i+=sprintf(&buf[i],",F%d float", j+1);
 	i+=sprintf(&buf[i],");"); buf[i]=0;
 	db.dbQuery(buf);
 
-	i=sprintf(buf,"create index if not exists idx_%s on %s(t);",tableName, tableName); // MariaDB
+    i=sprintf(buf,"show indexes from %s;", tableName);
     db.dbQuery(buf);
-};
+
+    if( !db.result->numRowsAffected() ){
+        i=sprintf(buf,"create index idx_%s on %s(t);",tableName, tableName);
+        db.dbQuery(buf);	
+    }
+}
